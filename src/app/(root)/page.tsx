fa007e8fee76b77d7b6ea97623fb2c4fd58e2a99
panoskouff <me@panoskouf.xyz>
2024-02-16 11:04:02 +0200

    add page wrapper and prototype grid

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,27 @@
 import styles from "./home.module.scss";
+import { PageWrapper } from "../components/PageWrapper/PageWrapper";
 
-export default function HomeContent() {
-  return <div className={styles.text}>homepage</div>;
+export default function HomePage() {
+  return (
+    <PageWrapper>
+      <div className={styles.gridContainer}>
+        <div className={styles.gridItem}>Name</div>
+        <div className={styles.gridItem}>Description</div>
+        <div className={styles.gridItem}>Price</div>
+        <div className={styles.gridItem}>
+          <div className={styles.textContainer}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+            incidunt! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Accusantium, atque!
+          </div>
+        </div>
+        <div className={styles.gridItem}>
+          <div className={styles.textContainer}>Lorem, ipsum.</div>
+        </div>
+        <div className={styles.gridItem}>
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </div>
+      </div>
+    </PageWrapper>
+  );
 }
---
+++ src/app/components/PageWrapper/PageWrapper.tsx
@@ -0,0 +1,9 @@
+import styles from "./PageWrapper.module.scss";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const PageWrapper: React.FC<Props> = ({ children }) => (
+  <div className={styles.container}>{children}</div>
+);
