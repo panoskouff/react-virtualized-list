abda5cc79168d1eb1c079f678e3a7a12276ef69a
panoskouff <me@panoskouf.xyz>
2024-02-16 12:16:37 +0200

    add page title and button

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -6,6 +6,16 @@ import { FixedGridRow } from "../components/FixedGridRow/FixedGridRow";
 export default function HomePage() {
   return (
     <PageWrapper>
+      <div className={styles.spacing}>
+        <div className={styles.flex}>
+          <div className={styles.spacer} />
+          <div className={styles.title}>Virtualized list</div>
+          <button className={styles.addButton} type="button">
+            Add new item
+          </button>
+        </div>
+      </div>
+
       <div className={styles.gridContainer}>
         <FixedGridTitleRow
           name="Name"
