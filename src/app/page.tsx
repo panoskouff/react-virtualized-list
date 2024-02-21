58a6f9ffff9c56ccf8fb3e8392a1ff55531f8080
panoskouff <me@panoskouf.xyz>
2024-02-21 13:11:41 +0200

    fix: make page dynamic and set 200k rows for production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import styles from '#/app/page.module.scss'
 import { Suspense } from 'react'
 import { FetchAndDisplayProductList } from '#/views/ProductList/FetchAndDisplayProductList'
 
+import { unstable_noStore as noStore } from 'next/cache'
+
 export default async function HomePage() {
+  noStore()
+
   return (
     <Suspense
       fallback={<div className={styles.loading}>Loading product data...</div>}
--- src/utils/fetchFakeProducts.ts
+++ src/utils/fetchFakeProducts.ts
@@ -1,12 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { ProductRow } from '#/types'
 
-/* we use only 50000 rows in production since vercel blocks
-our deployment since our response is higher than 10mb 
-https://vercel.com/docs/errors/FALLBACK_BODY_TOO_LARGE */
-
-const isProduction = process.env.NODE_ENV === 'production'
-const initialLength = isProduction ? 50_000 : 200_000
+const initialLength = 200_000
 
 const products: ProductRow[] = Array.from(
   { length: initialLength },
