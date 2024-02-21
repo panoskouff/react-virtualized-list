84fef4d8de9c030a611a626b28211aed0f5d273e
panoskouff <me@panoskouf.xyz>
2024-02-21 11:59:42 +0200

    use 50k rows in production and 200k in dev mode

--- src/utils/fetchFakeProducts.ts
+++ src/utils/fetchFakeProducts.ts
@@ -1,8 +1,15 @@
 import { faker } from '@faker-js/faker'
 import { ProductRow } from '#/types'
 
+/* we use only 50000 rows in production since vercel blocks
+our deployment since our response is higher than 10mb 
+https://vercel.com/docs/errors/FALLBACK_BODY_TOO_LARGE */
+
+const isProduction = process.env.NODE_ENV === 'production'
+const initialLength = isProduction ? 50_000 : 200_000
+
 const products: ProductRow[] = Array.from(
-  { length: 50 * 1000 },
+  { length: initialLength },
   (_, index) => ({
     id: faker.string.uuid(),
     data: [
